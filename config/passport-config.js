f420a3db62032cf2fbbaf3fddd7e79da8268ffdb
Eric <eheitmuller@gmail.com>
2016-05-09 08:43:30 +0000

    missed some files on last commit

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,2 +1,51 @@
 var LocalStrategy   = require('passport-local').Strategy;
 var User            = require('../models/user');
+
+
+module.exports = function(passport) {
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+    
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+    
+    passport.use('local-signup', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) {
+    
+        process.nextTick(function() {
+             User.findOne({ 'local.email' :  email }, function(err, user) {
+                 if (err) {
+                    return done(err);
+                 }
+                 
+                 if (user) {
+                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                 }
+                 
+                 var newUser = new User();
+
+                // set the user's local credentials
+                newUser.local.email    = email;
+                newUser.local.password = newUser.generateHash(password);
+
+                // save the user
+                newUser.save(function(err) {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+             });
+        });
+    }));
+    
+};
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,25 @@
 
 module.exports = function(routes, passport) {
   
-  
+    routes.get('/', function(req, res) {
+        res.render('login.ejs'); // load the index.ejs file
+    });
+    
+    routes.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+    
+    routes.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/game', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+    
+    routes.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
     
 };
